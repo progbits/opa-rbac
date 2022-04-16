ca099a050f47669214ab7ca2760a9f9ffcdefc50
progbits <53880655+progbits@users.noreply.github.com>
2022-04-16 11:55:57 +0100

    Add project scoped roles to schema

--- database/migration.sql
+++ database/migration.sql
@@ -1,19 +1,25 @@
+CREATE TABLE rbac_project (
+    id   INTEGER PRIMARY KEY,
+    name TEXT
+);
+
 CREATE TABLE rbac_user (
-    id INTEGER
+    id INTEGER PRIMARY KEY
 );
 
 CREATE TABLE rbac_role (
-    id        INTEGER,
+    id        INTEGER PRIMARY KEY,
     role_name TEXT
 );
 
-CREATE TABLE rbac_user_role (
-    rbac_user_id INTEGER,
-    rbac_role_id INTEGER
+CREATE TABLE rbac_project_user_role (
+    rbac_project_id INTEGER,
+    rbac_user_id    INTEGER,
+    rbac_role_id    INTEGER
 );
 
 CREATE TABLE rbac_permission (
-    id              INTEGER,
+    id              INTEGER PRIMARY KEY,
     permission_name TEXT
 );
 
