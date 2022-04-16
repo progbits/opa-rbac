32ec7bb18def245dde9b7a742f20d16b8d757c41
progbits <53880655+progbits@users.noreply.github.com>
2022-04-16 13:03:16 +0100

    Add basic HTTP server and /v1/check endpoint

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"errors"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -14,10 +16,13 @@ const (
 )
 
 type Server struct {
-	db *sql.DB
+	db         *sql.DB
+	httpServer *http.Server
 }
 
 func NewServer() (*Server, error) {
+	server := &Server{}
+
 	databaseFilePath := os.Getenv(databaseFilePathEnvVar)
 	if databaseFilePath == "" {
 		return nil, errors.New(fmt.Sprintf("%s not set", databaseFilePathEnvVar))
@@ -27,13 +32,51 @@ func NewServer() (*Server, error) {
 	if err != nil {
 		return nil, err
 	}
+	server.db = db
 
-	return &Server{db: db}, nil
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/check", server.handle)
+
+	httpServer := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+	server.httpServer = httpServer
+
+	return server, nil
+}
+
+func (s *Server) Start() {
+	log.Fatal(s.httpServer.ListenAndServe())
+}
+
+type RbacCheckRequest struct {
+	UserId     string `json:"user_id,omitempty"`
+	Object     string `json:"object,omitempty"`
+	Permission string `json:"permission,omitempty"`
+}
+
+func (s *Server) handle(w http.ResponseWriter, r *http.Request) {
+	check := RbacCheckRequest{}
+	err := json.NewDecoder(r.Body).Decode(&check)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	log.Printf(
+		"Check request: Can '%s' '%s' '%s'?",
+		check.UserId,
+		check.Permission,
+		check.Object)
+	w.WriteHeader(http.StatusForbidden)
 }
 
 func main() {
-	_, err := NewServer()
+	server, err := NewServer()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	server.Start()
 }
