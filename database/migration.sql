29a8dd4c9289d9368d4564356a606a3509050dc7
progbits <53880655+progbits@users.noreply.github.com>
2022-04-17 21:41:07 +0100

    Update rbac_data view to return project user roles and role permissions

--- database/migration.sql
+++ database/migration.sql
@@ -30,22 +30,39 @@ CREATE TABLE rbac_role_permission (
 );
 
 CREATE VIEW IF NOT EXISTS rbac_data AS
-    WITH user_project_role AS (
+    WITH
+     user_project_role AS (
         SELECT rbac_user.name AS user_name,
                rbac_project.name AS project_name,
                json_group_array(rbac_role.name) AS roles
         FROM rbac_project_user_role
-        JOIN rbac_user ON rbac_user.id = rbac_project_user_role.rbac_user_id,
+                 JOIN rbac_user ON rbac_user.id = rbac_project_user_role.rbac_user_id,
              rbac_role ON rbac_role.id = rbac_project_user_role.rbac_role_id,
              rbac_project ON rbac_project.id = rbac_project_user_role.rbac_project_id
         GROUP BY rbac_user.name, rbac_project.name),
 
-        user_project_role_agg AS (
-             SELECT user_project_role.user_name AS user_name,
-                    json_group_object(
+     user_project_role_agg AS (
+         SELECT user_project_role.user_name AS user_name,
+                json_group_object(
                         user_project_role.project_name,
-                        user_project_role.roles) AS project_roles
-             FROM user_project_role
-             GROUP BY user_project_role.user_name)
+                        json(user_project_role.roles)) AS project_roles
+         FROM user_project_role GROUP BY user_project_role.user_name),
 
-    SELECT json_group_object(user_name, project_roles) FROM user_project_role_agg;
+     role_permissions AS (
+         SELECT rbac_role.name AS name,
+                json_group_array(rbac_permission.name) AS permissions
+         FROM rbac_role_permission
+                  JOIN rbac_role ON rbac_role_permission.rbac_role_id = rbac_role.id,
+              rbac_permission ON rbac_permission.id = rbac_role_permission.rbac_permission_id
+         GROUP BY rbac_role.name),
+
+     roles_object AS (
+         SELECT json_group_object(user_name, json(project_roles)) as object from user_project_role_agg),
+
+     roles_permissions_object AS (
+         SELECT json_group_object(name, json(permissions)) as object from role_permissions)
+
+    SELECT json_object(
+        'roles', json((SELECT object FROM roles_object)),
+        'permissions', json((SELECT object from roles_permissions_object))
+    ) AS data;
